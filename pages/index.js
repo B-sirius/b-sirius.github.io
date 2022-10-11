23c648580199ac6417b38f809d78de27587a3ad4
芭娜娜 <banana@bananadeMacBook-Pro.local>
2022-10-11 11:01:16 +0800

    feat: basic setup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
-    <div>home</div>
+    <div>
+      <div>home</div>
+      <div>
+        <Link href="/posts">posts</Link>
+      </div>
+    </div>
   )
 }
