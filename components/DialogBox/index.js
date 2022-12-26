820bfc442ef64f3612207000074688a058c7cac1
Yi Zhou <yizhou1@WKMZT333F147.global.publicisgroupe.net>
2022-12-26 17:59:53 +0800

    feat: welcome our new style

---
+++ components/DialogBox/index.js
@@ -0,0 +1,31 @@
+// 对话框组件，仿照了RPG游戏中的对话框样式
+
+import styled from 'styled-components';
+
+const DialogBox = ({ className, children }) => (
+    <div className={className}>
+        <div>
+            {children}
+        </div>
+    </div>
+)
+
+const StyledDialogBox = styled(DialogBox)`
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    border: 1px solid #9d701d;
+    background-color: #262427;
+    padding: 1px;
+    
+    & > div {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+        border: 1px solid #9d701d;
+        color: #fff;
+        padding: 5px;
+    }
+`
+
+export default StyledDialogBox;
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,11 @@
 import postMap from 'postMap.json';
 import dayjs from 'dayjs';
 import Link from 'next/link';
+import styled from 'styled-components';
+import StyledDialogBox from 'components/DialogBox';
 
-const list = Object
+// 按时间排序好的博客内容
+const sortedPosts = Object
     .values(postMap)
     .sort((post1, post2) => {
         const date1 = dayjs(post1.date);
@@ -13,18 +16,67 @@ const list = Object
         return -1;
     });
 
+const RootWrapper = styled.div`
+    background-color: #000;
+    overflow: scroll;
+`
+
+const Root = ({ children }) => (
+    <RootWrapper>
+        {children}
+    </RootWrapper>
+)
+
+const ContainerWrapper = styled.div`
+    width: 960px;
+    margin: auto;
+`
+
+const Container = ({ children }) => (
+    <ContainerWrapper>
+        {children}
+    </ContainerWrapper>
+)
+
+const StyledSectionTitle = styled(StyledDialogBox)`
+    width: fit-content;
+
+    & > div {
+        width: fit-content;
+    }
+`
+
+const HomeSection = ({ className, title, children }) => (
+    <div className={className}>
+        <StyledSectionTitle>
+            {title}
+        </StyledSectionTitle>
+        <StyledDialogBox>
+            {children}
+        </StyledDialogBox>
+    </div>
+)
+
+const StyledHomeSection = styled(HomeSection)`
+    display: flex;
+    flex-direction: column;
+    margin: 20px 0;
+`
+
 const PostList = () => (
-    <ul>
-        {list.map((post) => {
-            const { date, title, id } = post;
-            return (
-                <div key={id}>
-                    <Link href={`/posts/${id}`}>{title}</Link>
-                    <div>{date}</div>
-                </div>
-            )
-        })}
-    </ul>
+    <Root>
+        <Container>
+            {sortedPosts.map((post) => {
+                const { date, title, id } = post;
+                return (
+                    <StyledHomeSection title={title} key={id}>
+                        <Link href={`/posts/${id}`}>{title}</Link>
+                        <div>{date}</div>
+                    </StyledHomeSection>
+                )
+            })}
+        </Container>
+    </Root>
 )
 
 export default PostList;
