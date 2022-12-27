f49669429663afe01ce74f48f70e516fce1b3586
Yi Zhou <yizhou1@WKMZT333F147.global.publicisgroupe.net>
2022-12-27 15:05:37 +0800

    feat: post id

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,10 +3,12 @@ import fse from 'fs-extra';
 import matter from 'gray-matter';
 import path from 'path';
 import ReactMarkdown from 'react-markdown';
+import styled from 'styled-components';
+import StyledDialogBox from 'components/DialogBox';
 
 const postsDirPath = path.join(process.cwd(), '_posts');
 
-export async function getStaticPaths(context) {
+export async function getStaticPaths() {
     return {
         paths: Object.keys(postMap).map(id => ({
             params: { id }
@@ -30,20 +32,39 @@ export async function getStaticProps(context) {
     }
 }
 
+// 根容器，覆盖全屏
+const RootWrapper = styled.div`
+    background-color: #000;
+    overflow: scroll;
+`
+const Root = ({ children }) => (
+    <RootWrapper>
+        {children}
+    </RootWrapper>
+)
+
+const ContentBox = styled(StyledDialogBox)`
+    margin: 40px;
+`
+
 const Post = (props) => {
     const { mdText, date, title } = props;
     return (
-        <div>
-            <div>
-                title: {title}
-            </div>
-            <div>
-                date: {date}
-            </div>
-            <ReactMarkdown>
-                {mdText}
-            </ReactMarkdown>
-        </div>
+        <Root>
+            <ContentBox>
+                <div>
+                    title: {title}
+                </div>
+                <div>
+                    date: {date}
+                </div>
+            </ContentBox>
+            <ContentBox>
+                <ReactMarkdown>
+                    {mdText}
+                </ReactMarkdown>
+            </ContentBox>
+        </Root>
     )
 }
 
