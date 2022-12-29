9b8e87fb051baeabdba892dbd5d380d78ff2e7bd
Yi Zhou <yizhou1@WKMZT333F147.global.publicisgroupe.net>
2022-12-29 15:43:22 +0800

    feat: custom safari theme-color

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Head from 'next/head';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>LWDW!</title>
+        <meta name="theme-color" content="#000" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
