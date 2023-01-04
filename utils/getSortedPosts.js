20cb695e40f4b560511257e1fbc13958036f2c77
Yi Zhou <yizhou1@WKMZT333F147.global.publicisgroupe.net>
2023-01-04 15:06:46 +0800

    fix: sorted posts

--- utils/getSortedPosts.js
+++ utils/getSortedPosts.js
@@ -1,16 +1,17 @@
 import postMap from 'postMap.json';
 import dayjs from 'dayjs';
 
-// 按时间排序好的博客内容
-const sortedPosts = Object
-    .values(postMap)
-    .sort((post1, post2) => {
-        const date1 = dayjs(post1.date);
-        const date2 = dayjs(post2.date);
-        if (date1.isBefore(date2)) {
-            return 1;
-        }
-        return -1;
-    });
-
-export default sortedPosts;
+export default function getSortedPosts() {
+    // 按时间排序好的博客内容
+    const sortedPosts = Object
+        .values(postMap)
+        .sort((post1, post2) => {
+            const date1 = dayjs(post1.date);
+            const date2 = dayjs(post2.date);
+            if (date1.isBefore(date2)) {
+                return 1;
+            }
+            return -1;
+        });
+    return sortedPosts;
+}
