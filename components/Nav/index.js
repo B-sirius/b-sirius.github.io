4c26220a6b08d7131bf3b17801a6a6e130c2e0ff
Yi Zhou <yizhou1@WKMZT333F147.global.publicisgroupe.net>
2023-01-04 11:57:40 +0800

    feat: update nav link

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,10 +1,12 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Link from 'next/link';
 import BlogTitle from 'components/BlogTitle';
+import InboundLink from 'components/InboundLink';
 
 const LINKS = [
     ['Home', '/'],
     ['Posts', '/posts'],
+    ['Github', 'https://github.com/B-sirius', true],
 ];
 
 const LinkContainer = styled.div`
@@ -16,7 +18,7 @@ const Container = styled.div`
     margin-bottom: 50px;
 `
 
-const StyledLink = styled(Link)`
+const linkStyle = css`
     padding: 5px 0;
     font-family: monospace;
     color: ${(props) => (props.$actived ? 'var(--nav-color)' : 'var(--font-color)')};
@@ -28,15 +30,22 @@ const StyledLink = styled(Link)`
     }
 `;
 
+const StyledLink = styled(Link)`${linkStyle}`
+
+const StyledInboundLink = styled(InboundLink)`${linkStyle}`
+
 const Nav = ({ className, activeIndex }) => (
     <Container className={className}>
         <LinkContainer>
             {
-                LINKS.map((item, index) => (
-                    <StyledLink href={item[1]} key={item[0]} $actived={index === activeIndex}>
-                        [{item[0]}]
-                    </StyledLink>
-                ))
+                LINKS.map((item, index) => {
+                    const LinkComponent = item[2] ? StyledInboundLink : StyledLink;
+                    return (
+                        <LinkComponent href={item[1]} key={item[0]} $actived={index === activeIndex}>
+                            [{item[0]}]
+                        </LinkComponent>
+                    );
+                })
             }
         </LinkContainer>
         <BlogTitle />
