e3d41f53da9d9500b2990a09a2fea60febac1de5
Yi Zhou <yizhou1@WKMZT333F147.global.publicisgroupe.net>
2023-02-07 21:46:52 +0800

    feat: update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff" />
+                <meta name="msapplication-TileColor" content="#ffffff" />
                 <meta name="theme-color" content="#000" />
                 <meta property="og:type" content="website" />
                 <meta property="og:image" content="https://s2.loli.net/2023/01/18/42YTZxzePtR7jy9.png" />
